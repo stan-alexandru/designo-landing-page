8784c3f08ed3fd097e545ca4ce1668dc1d5fa1f3
stan-alexandru <stan.alexandru26@gmail.com>
2024-03-31 01:39:47 +0200

    feat:add footer and header

---
+++ src/data/index.ts
@@ -0,0 +1,72 @@
+export const HEADER_DATA = [
+	{ title: 'Our Company', href: '/about', ariaLabel: 'About' },
+	{ title: 'Locations', href: '/locations', ariaLabel: 'Locations' },
+	{ title: 'Contact', href: '/contact', ariaLabel: 'Contact' },
+];
+export const SOCIAL_DATA = [
+	{
+		title: 'Facebook',
+		href: 'https://www.facebook.com',
+		path: '/assets/shared/desktop/icon-facebook.svg',
+	},
+	{
+		title: 'Youtube',
+		href: 'https://www.youtube.com',
+		path: '/assets/shared/desktop/icon-youtube.svg',
+	},
+	{
+		title: 'Twitter',
+		href: 'https://www.twitter.com',
+		path: '/assets/shared/desktop/icon-twitter.svg',
+	},
+	{
+		title: 'Pinterest',
+		href: 'https://www.pinterest.com',
+		path: '/assets/shared/desktop/icon-pinterest.svg',
+	},
+	{
+		title: 'Instagram',
+		href: 'https://www.instagram.com',
+		path: '/assets/shared/desktop/icon-instagram.svg',
+	},
+];
+
+export const PROJECTS = [
+	{
+		title: 'WEB DESIGN',
+		href: '/projects/web-design',
+		path: '/assets/home/mobile/image-web-design.jpg',
+	},
+	{
+		title: 'APP DESIGN',
+		href: '/projects/app-design',
+		path: '/assets/home/mobile/image-app-design.jpg',
+	},
+	{
+		title: 'GRAPHIC DESIGN',
+		href: '/projects/graphic-design',
+		path: '/assets/home/mobile/image-graphic-design.jpg',
+	},
+];
+
+export const PERSONALITY = [
+	{
+		title: 'Passionate',
+		description:
+			'Each project starts with an in-depth brand research to ensure we only create products that serve a purpose. We merge art, design, and technology into exciting new solutions.',
+		path: '/assets/home/desktop/illustration-passionate.svg',
+	},
+	{
+		title: 'RESOURCEFUL',
+		description:
+			'Everything that we do has a strategic purpose. We use an agile approach in all of   projects and value customer collaboration. It guarantees superior results that fulfill our clients’ needs.',
+		path: '/assets/home/desktop/illustration-resourceful.svg',
+	},
+	{
+		title: 'FRIENDLY',
+		description:
+			'We are a group of enthusiastic folks who know how to put people first. Our success depends on our customers, and we strive to give them the best experience a company can provide.',
+
+		path: '/assets/home/desktop/illustration-friendly.svg',
+	},
+];
